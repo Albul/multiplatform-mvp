3a5a5b82784f5a55aaaf36b868736e09178d1b8d
albul <abdula.ua@gmail.com>
2020-02-12 18:43:29 +0200

    Add taskList tests

--- core-app/src/commonTest/kotlin/com/olekdia/mvpcore/presentation/TaskListPresenterTest.kt
+++ core-app/src/commonTest/kotlin/com/olekdia/mvpcore/presentation/TaskListPresenterTest.kt
@@ -2,27 +2,100 @@ package com.olekdia.mvpcore.presentation
 
 import com.olekdia.mvpcore.BaseTest
 import com.olekdia.mvpcore.TaskFilter
+import com.olekdia.mvpcore.ViewType
+import com.olekdia.mvpcore.presentation.views.IInputTaskView
+import com.olekdia.mvpcore.presentation.views.IMainView
 import com.olekdia.mvpcore.presentation.views.ITaskListView
-import io.mockk.*
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlin.test.*
 
 class TaskListPresenterTest : BaseTest() {
 
     val taskListViewMock = mockk<ITaskListView>(relaxed = true, relaxUnitFun = true) {
+        every { dataProvider } returns listOf()
         every { clearCompletedVisible } returns true
         every { isReady } returns true
         taskListPresenter.onAttach(this)
     }
 
+    val mainViewMock = mockk<IMainView>(relaxed = true, relaxUnitFun = true) {
+        mainViewPresenter.onAttach(this)
+    }
+
+    @Test
+    fun onChangeFilter_toIdenticalFilter_stateNotChanged() {
+        val stateBefore = taskModel.state
+        taskListPresenter.onChangeFilter(stateBefore.filter)
+        assertNotSame(stateBefore, taskModel.state)
+        verify(exactly = 0) { taskListViewMock.changeFilter(stateBefore.filter) }
+        verify(exactly = 0) { taskListViewMock.dataProvider = taskModel.state.filteredList }
+    }
+
     @Test
     fun onChangeFilter_filterIsApplied() {
+        val stateBefore = taskModel.state
         taskListPresenter.onChangeFilter(TaskFilter.COMPLETED)
         assertEquals(TaskFilter.COMPLETED, taskModel.state.filter)
 
         taskListPresenter.onChangeFilter(TaskFilter.ALL)
         assertEquals(TaskFilter.ALL, taskModel.state.filter)
 
+        assertNotSame(stateBefore, taskModel.state)
         verify { taskListViewMock.changeFilter(TaskFilter.ALL) }
+        verify { taskListViewMock.dataProvider = taskModel.state.filteredList }
+    }
+
+    @Test
+    fun onToggleCompleted_nonExistingPos_stateNotChanged() {
+        val stateBefore = taskModel.state
+        taskListPresenter.onToggleCompleted(Int.MAX_VALUE)
+
+        assertSame(stateBefore, taskModel.state)
+    }
+
+    @Test
+    fun onToggleCompleted_stateChanged_isCompletedOpposite() {
+        val stateBefore = taskModel.state
+        val taskBefore = stateBefore.filteredList!![0]
+        taskListPresenter.onToggleCompleted(0)
+
+        assertNotEquals(stateBefore, taskModel.state)
+        assertEquals(taskBefore.isCompleted, !taskModel.state.filteredList!![0].isCompleted)
+
+        verify { taskListViewMock.dataProvider = taskModel.state.filteredList }
+    }
+
+    @Test
+    fun onEdit_nonExistingPos_stateNotChanged() {
+        val stateBefore = taskModel.state
+        taskListPresenter.onEdit(Int.MAX_VALUE)
+
+        assertSame(stateBefore, taskModel.state)
+        verify(exactly = 0) { mainViewMock.showView(any(), any(), any()) }
+    }
+
+    @Test
+    fun onEdit_showViewIsCalled() {
+        val targetEntry = taskModel.state.filteredList!![0]
+        taskListPresenter.onEdit(0)
+
+        verify(exactly = 1) {
+            mainViewMock.showView(
+                IInputTaskView.COMPONENT_ID,
+                ViewType.FORM,
+                any()
+            )
+        }
+    }
+
+    @Test
+    fun onDelete_nonExistingPos_stateNotChanged_snackNotShowing() {
+        val stateBefore = taskModel.state
+        taskListPresenter.onDelete(Int.MAX_VALUE)
+
+        assertSame(stateBefore, taskModel.state)
+        //assertFalse(snackPresenter.)
     }
 }
